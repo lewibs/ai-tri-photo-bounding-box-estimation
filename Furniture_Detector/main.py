9270ecd94f9aa4654500cbc889a9732d0b8f188f
lewibs <benjaminsl2000@gmail.com>
2024-04-26 15:17:04 -0400

    removed ai and added FurnitureDetector

---
+++ Furniture_Detector/main.py
@@ -0,0 +1,40 @@
+import torch
+import torchvision.transforms as transforms
+from FurnitureDetector import get_furniture_detector
+from torchvision.datasets import CocoDetection
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from PIL import Image, ImageDraw
+
+# COCO class labels
+coco_labels = [
+    '__background__', 'object'
+]
+
+# Step 1: Load pre-trained Faster R-CNN model
+
+model = get_furniture_detector()
+model.load_state_dict(torch.load("./FurnitureDetector_weights_maskless.pth"))
+model.eval()
+
+# Step 2: Load the image
+image_path = "../dataset/PennFudanPed/PNGImages/FudanPed00001.png"
+image = Image.open(image_path).convert("RGB")
+
+# Step 3: Preprocess the image
+transform = transforms.Compose([transforms.ToTensor()])
+image_tensor = transform(image)
+
+# Step 4: Perform inference
+with torch.no_grad():
+    predictions = model([image_tensor])
+
+print(predictions)
+
+# Step 5: Display the image with bounding boxes and labels
+draw = ImageDraw.Draw(image)
+for box, label_id, score in zip(predictions[0]['boxes'], predictions[0]['labels'], predictions[0]['scores']):
+    label = coco_labels[int(label_id)]
+    draw.rectangle([(box[0], box[1]), (box[2], box[3])], outline="red")
+    draw.text((box[0], box[1]), f"{label} ({score:.2f})", fill="red")
+
+image.show()
